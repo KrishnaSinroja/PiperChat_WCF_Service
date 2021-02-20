afc9046a8e451899a464e14089ed981a66bd934d
Krishna Sinroja <krishnavsinroja@gmail.com>
2021-02-20 21:43:20 +0530

    Login successfully done

--- PiperchatService/Service/UserService.cs
+++ PiperchatService/Service/UserService.cs
@@ -1,4 +1,5 @@
-﻿using PiperchatService.Models;
+﻿using PiperchatService.Contract;
+using PiperchatService.Models;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -8,32 +9,38 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
 
-namespace PiperchatService.Contract
+namespace PiperchatService.Service
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, InstanceContextMode = InstanceContextMode.Single)]
     public class UserService : IUserService
     {
+       // Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = master; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False
 
-        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=PiperChatDb;Integrated Security=True";
+        string connectionString = @"Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = PiperChatDb; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         public string InsertUserRecord(User user)
         {
             string response = "";
             try
             {
-                string query = "INSERT INTO User (UserId,Name,Email,Password) values(@UserId, @Name, @Email, @Password)";
+
+                string query = "INSERT INTO dbo.UserTable(Name,Email,Password) values(@Name, @Email, @Password)";
                 SqlConnection con = new SqlConnection(connectionString);
+                con.Open();
                 SqlCommand cmd = new SqlCommand(query,con);
-                cmd.Parameters.AddWithValue("@UserId", user.UserId);
+                //cmd.Parameters.AddWithValue("@UserId", user.UserId);
+                Console.WriteLine(user.Name);
                 cmd.Parameters.AddWithValue("@Name", user.Name);
                 cmd.Parameters.AddWithValue("@Email", user.Email);
                 cmd.Parameters.AddWithValue("@Password", user.Password);
-                con.Open();
+               
                 cmd.ExecuteNonQuery();
                 con.Close();
                 response = "Inserted Successfully";
 
             }catch(Exception e)
             {
+                Console.WriteLine(e.Message);
                 response = "Error " + e;
             }
             return response;
@@ -96,7 +103,7 @@ namespace PiperchatService.Contract
             try
             {
                 SqlConnection con = new SqlConnection(connectionString);
-                string Query = "SELECT * FROM tblEmployee WHERE UserId=@UserId";
+                string Query = "SELECT * FROM User WHERE UserId=@UserId";
 
                 SqlDataAdapter sda = new SqlDataAdapter(Query, con);
                 sda.SelectCommand.Parameters.AddWithValue("@UserId", user.UserId);
@@ -109,7 +116,49 @@ namespace PiperchatService.Contract
             return ds;
            
         }
-       
-        
+
+        public string Validate(string email, string password)
+        {
+            string response="";
+            try
+            {
+
+
+                SqlConnection con = new SqlConnection(connectionString);
+                con.Open();
+                string Query = "SELECT * from dbo.UserTable where Email='"+email+"' and Password='"+password+"'";
+                
+                SqlCommand cmd = new SqlCommand();
+                cmd = new SqlCommand(Query, con);
+                /*cmd.Parameters.Add("@Email", SqlDbType.NVarChar);
+                cmd.Parameters["@Email"].Value = email;
+                cmd.Parameters.Add("@Password", SqlDbType.NVarChar);
+                cmd.Parameters["@Password"].Value = password;*/
+                //cmd.Parameters.AddWithValue("@Password", password);
+                //cmd.Parameters.AddWithValue("@Email", email);
+                SqlDataAdapter sda = new SqlDataAdapter(Query, con);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                int i = cmd.ExecuteNonQuery();
+             
+                if (dt.Rows.Count > 0)
+                {
+                    response = "Login Successfull";
+                }
+                else
+                {
+                    response = "Login Failed";
+                }
+                con.Close();
+            }
+            catch(Exception e)
+            {
+                
+                response = "Error" + e;
+            }
+            Console.WriteLine(response);
+            return response;
+            
+        }
     }
 }
